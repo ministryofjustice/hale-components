c117d366c8ef6896338ec2c30f1129da1692cd9f
RobLoweMOJ <51912126+RobLoweMOJ@users.noreply.github.com>
2024-03-13 10:56:05 +0000

    v 1.0.1

--- hale-components.php
+++ hale-components.php
@@ -4,7 +4,7 @@
 Plugin Name: Hale Components
 Description: Functions that are commonly used across the Hale Platform
 Author: Ministry of Justice
-Version: 1.0.0
+Version: 1.0.1
 */
 
 include 'inc/site-path-track.php'; 
