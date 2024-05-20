5d23784feecb48787b7d1c3873524e3a680c5e96
adam <brown-a2@users.noreply.github.com>
2024-05-20 17:09:11 +0100

    Improve readablity

--- moj-components/component/Head/HeadSettings.php
+++ moj-components/component/Head/HeadSettings.php
@@ -21,7 +21,7 @@ class HeadSettings extends Head
     {
         add_settings_field(
             'head_element',
-            __('Element', 'wp-moj-components'),
+            __('Add header code:', 'wp-moj-components'),
             [$this, 'addHeadElement'],
             'mojComponentSettings',
             $section
@@ -47,9 +47,9 @@ class HeadSettings extends Head
     {
         ?>
         <div class="welcome-panel-column">
-            <h4><?php _e('Add HTML elements to wp_head', 'wp-moj-components') ?></h4>
-            <p style="max-width: 600px"><?php _e('Add HTML meta-related elements to the head.
-            On WP multisite, this applies to the specific site your logged in as.
+            <h4><?php _e('Info', 'wp-moj-components') ?></h4>
+            <p><?php _e('Add HTML meta-related elements to the websites wp_head.
+            On WP multisite, this applies to the specific site your logged in as.<br><br>
             This is normally for validating an external app, testing a CDN script,
             or SEO tagging that cannot be done via Yoast.', 'wp-moj-components'); ?></p>
         </div>
