ab346dd33cf2e9211a6f284afff4767c6c5ceb2a
adam <brown-a2@users.noreply.github.com>
2024-05-20 16:51:12 +0100

    Add documenation to Taxonomy Updater page

--- moj-components/component/TaxonomyUpdater/TaxonomyUpdater.php
+++ moj-components/component/TaxonomyUpdater/TaxonomyUpdater.php
@@ -4,6 +4,11 @@ namespace MOJComponents\TaxonomyUpdater;
 class TaxonomyUpdater
 {
     public function hale_taxonomy_updater_tool() {
+
+        if (!current_user_can('manage_options')) {
+            wp_die(__('You do not have sufficient permissions to access this page.'));
+        }
+
         // Check if the user has submitted the form
         if (isset($_POST['update_taxonomy'])) {
     
@@ -38,15 +43,18 @@ class TaxonomyUpdater
     
         <div class="wrap">
             <h1>Taxonomy Updater</h1>
+            <p>Run a database update on the current site that changes a taxonomy entry in the database to a new one. 
+            <br>Make sure to use the exact taxonomy key of the taxonomy you want to change. 
+            <br>Once run, all content related to the old taxonomy will now <br>be associated with the new taxonomy name.</p>
             <form method="post" action="">
                 <?php wp_nonce_field('taxonomy_updater_action', 'taxonomy_updater_nonce'); ?>
                 <table class="form-table">
                     <tr valign="top">
-                        <th scope="row"><label for="old_slug">Old Taxonomy Slug</label></th>
+                        <th scope="row"><label for="old_slug">Taxonomy key:</label></th>
                         <td><input type="text" id="old_slug" name="old_slug" value="" required /></td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row"><label for="new_slug">New Taxonomy Slug</label></th>
+                        <th scope="row"><label for="new_slug">New taxonomy key:</label></th>
                         <td><input type="text" id="new_slug" name="new_slug" value="" required /></td>
                     </tr>
                 </table>
