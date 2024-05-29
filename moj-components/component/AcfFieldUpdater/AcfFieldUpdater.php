cd2eb8e54cb07d89b0a9986d099a10fe44b87232
adam <brown-a2@users.noreply.github.com>
2024-05-29 15:24:48 +0100

    Format

--- moj-components/component/AcfFieldUpdater/AcfFieldUpdater.php
+++ moj-components/component/AcfFieldUpdater/AcfFieldUpdater.php
@@ -1,73 +1,78 @@
 <?php
+
 namespace MOJComponents\AcfFieldUpdater;
 
 class AcfFieldUpdater
 {
     /**
-     * Displays a form in the WordPress admin to update meta keys or meta values..
+     * Displays a form in the WordPress admin to update meta keys or meta values.
      * 
      * This function provides an interface for site administrators to update meta keys or meta values stored in the
      * wp_postmeta table. Users can select whether they want to update a meta key or a meta value using a dropdown.
      * Depending on the selection, appropriate input fields are displayed. After submitting the form, the specified
      * updates are performed on the database.
      */
-    public function hale_acf_field_updater_tool() {
-
+    public function haleAcfFieldUpdaterTool()
+    {
         if (!current_user_can('manage_options')) {
             wp_die(__('You do not have sufficient permissions to access this page.'));
         }
-    
+
         // Check if the user has submitted the form
         if (isset($_POST['update_acf_field'])) {
-    
-            if (!isset($_POST['acf_field_updater_nonce']) || !wp_verify_nonce($_POST['acf_field_updater_nonce'], 'acf_field_updater_action')) {
+            if (
+                !isset($_POST['acf_field_updater_nonce']) ||
+                !wp_verify_nonce($_POST['acf_field_updater_nonce'], 'acf_field_updater_action')
+            ) {
                 die('Security check failed');
             }
-    
+
             global $wpdb;
-    
-            $update_type = sanitize_text_field($_POST['update_type']);
-            $old_value = sanitize_text_field($_POST['old_value']);
-            $new_value = sanitize_text_field($_POST['new_value']);
-            $meta_key = sanitize_text_field($_POST['meta_key']);
-    
-            if ($update_type === 'key') {
+
+            $updateType = sanitize_text_field($_POST['update_type']);
+            $oldValue = sanitize_text_field($_POST['old_value']);
+            $newValue = sanitize_text_field($_POST['new_value']);
+            $metaKey = sanitize_text_field($_POST['meta_key']);
+
+            if ($updateType === 'key') {
                 $sql = $wpdb->prepare(
                     "UPDATE {$wpdb->postmeta}
                     SET meta_key = %s
                     WHERE meta_key = %s",
-                    $new_value,
-                    $old_value
+                    $newValue,
+                    $oldValue
                 );
-            } else if ($update_type === 'value') {
+            } elseif ($updateType === 'value') {
                 $sql = $wpdb->prepare(
                     "UPDATE {$wpdb->postmeta}
                     SET meta_value = %s
                     WHERE meta_value = %s AND meta_key = %s",
-                    $new_value,
-                    $old_value,
-                    $meta_key
+                    $newValue,
+                    $oldValue,
+                    $metaKey
                 );
             }
-    
+
             // Execute the query
-            $updated_rows = $wpdb->query($sql);
-    
+            $updatedRows = $wpdb->query($sql);
+
             // Check for success
-            if ($updated_rows > 0) {
-                echo '<div class="updated"><p>ACF field updated successfully. Rows affected: ' . $updated_rows . '</p></div>';
+            if ($updatedRows > 0) {
+                echo '<div class="updated"><p>ACF field updated successfully. Rows affected: ' . $updatedRows . '</p></div>';
             } else {
                 echo '<div class="error"><p>No rows updated. It\'s possible the old meta key or value did not exist or was already updated.</p></div>';
             }
         }
         ?>
-    
+
         <div class="wrap">
             <h1>ACF Meta Field Updater</h1>
-            <p>Run a database update on the current site in the _postmeta table. 
-            <br>Select whether you want to update a meta key or meta value.
-            <br>Once run, all content related to the old meta key or value will
-            <br>be associated with the new meta key or value.</p>
+            <p>
+                Run a database update on the current site in the _postmeta table. 
+                <br>Select whether you want to update a meta key or meta value.
+                <br>Once run, all content related to the old meta key or value will
+                <br>be associated with the new meta key or value.
+            </p>
             <form method="post" action="">
                 <?php wp_nonce_field('acf_field_updater_action', 'acf_field_updater_nonce'); ?>
                 <table class="form-table">
