d7e0ddd11b641bda55d5e30297974d99316ecd4d
Malcolm.Butler <malcolm.butler@digital.justice.gov.uk>
2025-02-07 15:47:31 +0000

    Added new filter to allow unfiltered HTML for all editor rôles

--- moj-components/component/Users/RoleHooks.php
+++ moj-components/component/Users/RoleHooks.php
@@ -66,6 +66,19 @@ class RoleHooks
     }
 
     /**
+     * Filter allowUnfilteredHTMLforAllEditors
+     * Map meta capabilities to capabilities
+     * Allows all editors to write unfiltered HTML, meaning iFrames and Script tags will no longer be stripped out
+     */
+    public static function allowUnfilteredHTMLforAllEditors($caps, $cap, $user_id) {
+        if ( 'unfiltered_html' === $cap && user_can( $user_id, 'editor' ) ) {
+            return [ 'unfiltered_html' ];
+        }
+
+        return $caps;
+    }
+
+    /**
      * Determine if the current user is allowed to edit/delete/manage the specified user.
      * Non-administrators cannot edit administrators.
      *
@@ -194,6 +207,7 @@ class RoleHooks
     {
         add_filter('editable_roles', __CLASS__ . '::filterEditableRoles', 10, 1);
         add_filter('map_meta_cap', __CLASS__ . '::filterPreventModificationOfAdminUser', 10, 4);
+        add_filter('map_meta_cap', __CLASS__ . '::allowUnfilteredHTMLforAllEditors', 10, 3);
         add_action('admin_menu', __CLASS__ . '::actionRestrictAppearanceThemesMenu', 999);
 
         // stop Editors
