807ebe97b6bf5344cf29d78b7ddbf76d8a008541
RobLoweMOJ <51912126+RobLoweMOJ@users.noreply.github.com>
2025-09-18 10:32:50 +0100

    v1.9.0

--- hale-components.php
+++ hale-components.php
@@ -4,7 +4,7 @@
 Plugin Name: Hale Components
 Plugin URI: https://github.com/ministryofjustice/hale-components
 Description: Functions that are commonly used across the Hale Platform.
-Version: 1.8.0
+Version: 1.9.0
 Author: Ministry of Justice
 Author URI: https://github.com/ministryofjustice
 Text Domain: hale-components
