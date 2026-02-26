2f9e195a490f676f57d908c5cc4bca1b3c7f9c78
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-02-26 15:49:55 +0000

    Invalidate cache on `delete_attachment`.

--- inc/cloudfront.php
+++ inc/cloudfront.php
@@ -22,7 +22,7 @@ use Aws\Exception\AwsException;
  *
  * @return WP_Post|false|null The filtered delete value, or false to prevent deletion.
  */
-function hale_components_invalidate_cloudfront_cache(WP_Post|false|null $delete, WP_Post $post)
+function hale_components_invalidate_cache_on_pre_delete_attachment(WP_Post|false|null $delete, WP_Post $post)
 {
 
     if ($delete === false) {
@@ -30,22 +30,12 @@ function hale_components_invalidate_cloudfront_cache(WP_Post|false|null $delete,
         return $delete;
     }
 
-    $attachment_url = wp_get_attachment_url($post->ID);
+    // Get the props, either `false` or `['is_cdn' => boolean, 'path' => string ]`
+    $attachment_props = hale_components_attachment_props_from_id($post->ID);
 
-    if (!$attachment_url) {
-        // For whatever reason, we don't have an attachment URL, do nothing.
-        return $delete;
-    }
-
-    $attachment_url_parts = parse_url($attachment_url);
-
-    if (!$attachment_url_parts || !isset($attachment_url_parts['host'], $attachment_url_parts['path'])) {
-        // The URL wasn't parsed successfully.
-        return $delete;
-    }
-
-    if (!hale_components_host_is_a_cdn($attachment_url_parts['host'])) {
-        // The attachment URL is not on a CDN, so do nothing.
+    if (!$attachment_props || !$attachment_props['is_cdn']) {
+        // The URL wasn't parsed successfully, 
+        // or the attachment URL is not on a CDN, so do nothing.
         return $delete;
     }
 
@@ -54,7 +44,7 @@ function hale_components_invalidate_cloudfront_cache(WP_Post|false|null $delete,
     try {
         // Trigger the invalidation - don't block the delete request by waiting for success.
         hale_components_invalidate_cloudfront_path(
-            $attachment_url_parts['path'],
+            $attachment_props['path'],
             'attachment-' . $post->ID
         );
         // If we are here, then the status is either InProgress or Completed.
@@ -74,8 +64,72 @@ function hale_components_invalidate_cloudfront_cache(WP_Post|false|null $delete,
     return $delete;
 };
 
-add_filter('pre_delete_attachment', 'hale_components_invalidate_cloudfront_cache', 100, 2);
+add_filter('pre_delete_attachment', 'hale_components_invalidate_cache_on_pre_delete_attachment', 100, 2);
 
+
+/**
+ * Fire off a second CloudFront invalidation request, 
+ * to ensure that the invalidation happens when the source file is NOT on S3.
+ *
+ * This will run after WP S3 Uploads has deleted the file from S3.
+ * This is because WP S3 Uploads uses this hook to delete the files, but with the default priority.
+ *
+ * @param int $post The attachment post ID.
+ * @return void
+ */
+function hale_components_invalidate_cache_on_delete_attachment(int $post_id)
+{
+
+    $attachment_props = hale_components_attachment_props_from_id($post_id);
+
+    if (!$attachment_props || !$attachment_props['is_cdn']) {
+        return;
+    }
+
+    try {
+        hale_components_invalidate_cloudfront_path(
+            $attachment_props['path'],
+            'delete_attachment-' . $post_id
+        );
+    } catch (Throwable $t) {
+        error_log($t->getMessage());
+    }
+};
+
+add_action('delete_attachment', 'hale_components_invalidate_cache_on_delete_attachment', 100, 1);
+
+/**
+ * Get attachment properties from an attachment ID.
+ *
+ * @param int $attachment_id
+ * @return false|array Returned array is of the type ['is_cdn' => boolean, 'path' => string ]
+ */
+function hale_components_attachment_props_from_id($attachment_id): array|false
+{
+
+    if (!$attachment_id) {
+        return false;
+    }
+
+    $attachment_url = wp_get_attachment_url($attachment_id);
+
+    if (!$attachment_url) {
+        // For whatever reason, we don't have an attachment URL, do nothing.
+        return false;
+    }
+
+    $attachment_url_parts = parse_url($attachment_url);
+
+    if (!$attachment_url_parts || !isset($attachment_url_parts['host'], $attachment_url_parts['path'])) {
+        // The URL wasn't parsed successfully.
+        return false;
+    }
+
+    return [
+        'is_cdn' => hale_components_host_is_a_cdn($attachment_url_parts['host']),
+        'path' => $attachment_url_parts['path'],
+    ];
+}
 
 /**
  * Check if a host is CloudFront.
@@ -161,12 +215,12 @@ function hale_components_invalidate_cloudfront_path(
             ],
         ]);
 
-        $invalidationId = $create['Invalidation']['Id'];
+        $invalidation_id = $create['Invalidation']['Id'];
 
         if (!$blocking) {
-            error_log("Invalidation initialised for {$path}. (Id: {$invalidationId})");
+            error_log("Invalidation initialised for {$path}. (Id: {$invalidation_id})");
             return [
-                'Id'         => $invalidationId,
+                'Id'         => $invalidation_id,
                 'Status'     => $create['Invalidation']['Status'],
                 'Path'       => $path,
                 'CreateTime' => $create['Invalidation']['CreateTime'],
@@ -175,7 +229,7 @@ function hale_components_invalidate_cloudfront_path(
 
 
         // ---- Poll until Completed or timeout ----
-        return hale_components_poll_cloudfront_invalidation($client, $distribution_id, $invalidationId, $timeout_seconds, $path);
+        return hale_components_poll_cloudfront_invalidation($client, $distribution_id, $invalidation_id, $timeout_seconds, $path);
     } catch (AwsException $e) {
         // If you reused the same callerReference with *different* paths,
         // CloudFront will throw an error; surface that clearly.
@@ -190,7 +244,7 @@ function hale_components_invalidate_cloudfront_path(
  *
  * @param CloudFrontClient $client          The configured CloudFront client.
  * @param string           $distribution_id The CloudFront distribution ID.
- * @param string           $invalidationId  The invalidation ID to poll.
+ * @param string           $invalidation_id  The invalidation ID to poll.
  * @param int              $timeout_seconds Maximum seconds to wait before timing out.
  * @param string           $path            The path being invalidated (for logging).
  *
@@ -201,7 +255,7 @@ function hale_components_invalidate_cloudfront_path(
 function hale_components_poll_cloudfront_invalidation(
     CloudFrontClient $client,
     string $distribution_id,
-    string $invalidationId,
+    string $invalidation_id,
     int $timeout_seconds,
     string $path
 ): array {
@@ -210,7 +264,7 @@ function hale_components_poll_cloudfront_invalidation(
         sleep(5);
         $desc = $client->getInvalidation([
             'DistributionId' => $distribution_id,
-            'Id' => $invalidationId,
+            'Id' => $invalidation_id,
         ]);
         $status = $desc['Invalidation']['Status'];
         error_log("Status: {$status}");
@@ -220,10 +274,10 @@ function hale_components_poll_cloudfront_invalidation(
         }
     } while ($status !== 'Completed');
 
-    error_log("Invalidation completed for {$path}. (Id: {$invalidationId})");
+    error_log("Invalidation completed for {$path}. (Id: {$invalidation_id})");
 
     return [
-        'Id'         => $invalidationId,
+        'Id'         => $invalidation_id,
         'Status'     => $status,
         'Path'       => $path,
         'CreateTime' => $desc['Invalidation']['CreateTime'],
