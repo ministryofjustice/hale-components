ed0ca7b1a3f8414ac7fbb82acb86897efcf52394
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-04-29 09:27:07 +0100

    Revise based on copilot review

--- inc/symlink.php
+++ inc/symlink.php
@@ -1,6 +1,9 @@
 <?php
 
 defined('ABSPATH') || exit;
+
+add_filter('plugins_url', 'hale_components_reformat_plugin_urls', 10, 3);
+
 /**
  * Replace symlink mount path (/mnt/dev/) with target path (/var/www/html/wp-content/).
  *
@@ -11,16 +14,30 @@ defined('ABSPATH') || exit;
  * - A find and replace is used to reformat the `$plugin` value.
  * - plugins_url is called with this reformatted value.
  * - This URL is returned.
+ *
+ * @param string $url    The complete URL to the plugins directory including scheme and path.
+ * @param string $path   Path relative to the URL to the plugins directory. Blank string
+ *                       if no path is specified.
+ * @param string $plugin The plugin file path to be relative to. Blank string if no plugin
+ *                       is specified.
  */
-add_filter('plugins_url', function ($url, $path, $plugin) {
+function hale_components_reformat_plugin_urls ($url, $path, $plugin) {
     // If $plugin doesn't start with the mount path, then do nothing.
     if (!str_starts_with($plugin, '/mnt/dev/')) {
         return $url;
     }
+        
+    // Replace symlink mount path (/mnt/dev) with target path (/var/www/html/wp-content).
+    $plugin_reformatted = str_replace('/mnt/dev', WP_CONTENT_DIR, $plugin);
 
-    // Replace symlink mount path (/mnt/dev/) with target path (/var/www/html/wp-content/).
-    $plugin_reformatted = str_replace('/mnt/dev/', '/var/www/html/wp-content/', $plugin);
-
+    // Remove the filter, so that we never enter an infinite loop.
+    remove_filter('plugins_url', 'hale_components_reformat_plugin_urls', 10, 3);
+    
     // Recall plugins_url with the reformatted path.
-    return plugins_url($path, $plugin_reformatted);
-}, 10, 3);
+    $reformatted_url = plugins_url($path, $plugin_reformatted);
+    
+    // Restore the filter, following the previous removal.
+    add_filter('plugins_url', 'hale_components_reformat_plugin_urls', 10, 3);
+
+    return $reformatted_url;
+}
